47e388d21ba5ef1ec2dad1cb441038c839930185
Kyle Sunday <kyle.sunday@yahoo.com>
2024-10-07 19:50:21 -0600

    trying to fix install button

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,29 +1,31 @@
-const butInstall = document.getElementById('buttonInstall');
+const btnInstall = document.getElementById('buttonInstall');
+
+let deferredPrompt;
 
 // Logic for installing the PWA
 window.addEventListener('beforeinstallprompt', (event) => {
-  event.preventDefault();
-  deferredPrompt = event;
+    event.preventDefault();
+    deferredPrompt = event;
 });
 
 
 btnInstall.addEventListener('click', async () => {
-  if (deferredPrompt) {
-      deferredPrompt.prompt();
-      const { outcome } = await deferredPrompt.userChoice;
-      if (outcome === 'accepted') {
-          console.log('User accepted the install prompt');
-      } else {
-          console.log('User dismissed the install prompt');
-      }
-      deferredPrompt = null;
-      btnInstall.style.display = "none";
-  }
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        if (outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+        } else {
+            console.log('User dismissed the install prompt');
+        }
+        deferredPrompt = null;
+        btnInstall.style.display = "none";
+    }
 });
 
 
 window.addEventListener('appinstalled', (event) => {
-  event.preventDefault();
-  deferredPrompt = null;
-  console.log('PWA was installed');
+    event.preventDefault();
+    deferredPrompt = null;
+    console.log('PWA was installed');
 });
